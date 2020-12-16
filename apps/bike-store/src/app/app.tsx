0b2e416cabcc3b47ca493356a03701260364b216
Matthew Gathercole <30979900+MattJRG@users.noreply.github.com>
2020-12-16 09:12:12 +0000

    Update app.tsx

--- apps/bike-store/src/app/app.tsx
+++ apps/bike-store/src/app/app.tsx
@@ -22,9 +22,6 @@ export const App = () => {
       <main>
         <h2>Resources &amp; Tools</h2>
         <p>Isn't this great!? Thank you for using and showing some ♥ for Nx.</p>
-        <span>Does this work too?</span>
-        <h3>We're still working on this section for now... and we hope to release a new update soon</h3>
-        <button>Test me!</button>
         <div className="flex github-star-container">
           <a
             href="https://github.com/nrwl/nx"
